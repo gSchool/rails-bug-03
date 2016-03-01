2fc51a5e582d0fe5af3840eb596a06ce6faecb57
Simon Sekhon <simonsekhon@gmail.com>
2016-03-01 15:03:41 -0800

    validation file updated for the email field

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,19 +7,16 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
+  @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation, :name))
+  if @user.save
+    session[:user] = @user.id
+    if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
       redirect_to root_path
-    else
+    end
+  else
       render :new
     end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 
 end
