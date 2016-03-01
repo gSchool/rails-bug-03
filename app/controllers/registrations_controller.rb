11f71a96c86a719cf5e5f7d1817260607570081c
Simon Sekhon <simonsekhon@gmail.com>
2016-03-01 15:06:20 -0800

    changed controller back to the original code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,16 +7,19 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-  @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation, :name))
-  if @user.save
-    session[:user] = @user.id
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.new(user_params)
+    if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
-    end
-  else
+    else
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 
 end
