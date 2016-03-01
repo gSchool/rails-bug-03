82a31c9c2781431d9435b637873eca343acc4a5e
Lissa Walzer <lissawalzer@gmail.com>
2016-03-01 14:46:44 -0800

    Added email form validation and a test

--- spec/features/auth_spec.rb
+++ spec/features/auth_spec.rb
@@ -16,4 +16,16 @@ feature 'Auth' do
     expect(page).to have_content("user@example.com")
   end
 
+  scenario 'An error is shown if the user forgets to register with an email' do
+    visit root_path
+    within(".auth") { click_on "Register"}
+    expect(page).to have_content("Register")
+
+    fill_in "Name", with: "test"
+    fill_in "Password", with: "password"
+    fill_in "Confirm", with: "password"
+    within(".registration-form") { click_on "Register" }
+    expect(page).to have_content("Email can't be blank")
+end
+
 end
